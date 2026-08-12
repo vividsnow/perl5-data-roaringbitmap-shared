vividsnow/perl5-data-radixtree-shared

Pre-release fixes and documentation corrections

Shared.xs
@@ -10,6 +10,7 @@
         croak("Expected a Data::RadixTree::Shared object"); \
     RdxHandle *h = INT2PTR(RdxHandle*, SvIV(SvRV(sv))); \
     if (!h) croak("Attempted to use a destroyed Data::RadixTree::Shared object"); \
+    RdxHandle *h0 = h; PERL_UNUSED_VAR(h0); \
     sv_2mortal(SvREFCNT_inc(SvRV(sv)))
 
 /* Re-read the handle after a call that can run Perl code (tied/overloaded
@@ -19,8 +20,10 @@
  * explicit DESTROY, so the local `h` would dangle.  Used only where magic
  * can actually intervene between EXTRACT and the first use of h. */
 #define REEXTRACT(sv) \
+    if (!SvROK(sv)) \
+        croak("Data::RadixTree::Shared object was replaced during the call"); \
     h = INT2PTR(RdxHandle*, SvIV(SvRV(sv))); \
-    if (!h) croak("Data::RadixTree::Shared object destroyed during the call")
+    if (h != h0) croak("Data::RadixTree::Shared object replaced or destroyed during the call")
 
 #define MAKE_OBJ(class, handle) \
     SV *obj = newSViv(PTR2IV(handle)); \
@@ -50,7 +53,7 @@ new(class, path = &PL_sv_undef, node_capacity = 4096, arena_capacity = 65536, ..
      * SvPV_nolen(path) returns, so the capture must stay last. */
     const char *p = (SvGETMAGIC(path), SvOK(path)) ? SvPV_nolen(path) : NULL;
     RdxHandle *h = rdx_create(p, (uint64_t)node_capacity, (uint64_t)arena_capacity, mode, errbuf);   /* validates args into errbuf */
-    if (!h) croak("Data::RadixTree::Shared->new: %s", errbuf);
+    if (!h) croak("Data::RadixTree::Shared->new: %s", errbuf[0] ? errbuf : "out of memory");
     MAKE_OBJ(class, h);
   OUTPUT:
     RETVAL
@@ -66,7 +69,7 @@ new_memfd(class, name = &PL_sv_undef, node_capacity = 4096, arena_capacity = 655
   CODE:
     const char *nm = (SvGETMAGIC(name), SvOK(name)) ? SvPV_nolen(name) : NULL;   /* undef -> default label */
     RdxHandle *h = rdx_create_memfd(nm, (uint64_t)node_capacity, (uint64_t)arena_capacity, errbuf);   /* validates args into errbuf */
-    if (!h) croak("Data::RadixTree::Shared->new_memfd: %s", errbuf);
+    if (!h) croak("Data::RadixTree::Shared->new_memfd: %s", errbuf[0] ? errbuf : "out of memory");
     MAKE_OBJ(class, h);
   OUTPUT:
     RETVAL
@@ -79,7 +82,28 @@ new_from_fd(class, fd)
     char errbuf[RDX_ERR_BUFLEN];
   CODE:
     RdxHandle *h = rdx_open_fd(fd, errbuf);
-    if (!h) croak("Data::RadixTree::Shared->new_from_fd: %s", errbuf);
+    if (!h) croak("Data::RadixTree::Shared->new_from_fd: %s", errbuf[0] ? errbuf : "out of memory");
+    MAKE_OBJ(class, h);
+  OUTPUT:
+    RETVAL
+
+SV *
+new_readonly(class, path)
+    const char *class
+    SV *path
+  PREINIT:
+    char errbuf[RDX_ERR_BUFLEN];
+  CODE:
+    /* Open a FROZEN (sealed) file read-only: O_RDONLY + PROT_READ, no lock
+       ever.  A sealed file is immutable and no read path writes the mapping,
+       so queries take no reader-slot / rwlock traffic and any number of
+       processes can share one PROT_READ mapping (same architecture; the
+       magic rejects a wrong-endian file). */
+    const char *p = (SvGETMAGIC(path), SvOK(path)) ? SvPV_nolen(path) : NULL;
+    if (!p) croak("Data::RadixTree::Shared->new_readonly: path is required");
+    RdxHandle *h = rdx_open_readonly(p, errbuf);
+    if (!h) croak("Data::RadixTree::Shared->new_readonly: %s", errbuf);
+    class = SvPV_nolen(ST(0));   /* re-read: SvGETMAGIC(path) above can run Perl that reallocs/frees ST(0)'s PV */
     MAKE_OBJ(class, h);
   OUTPUT:
     RETVAL
@@ -104,11 +128,13 @@ insert(self, key, value = 1)
     const char *kp;
     int isnew;
   CODE:
+    if (h->readonly) croak("Data::RadixTree::Shared->insert: tree is frozen (read-only)");
     /* Resolve key bytes BEFORE locking: SvPVbyte croaks on wide chars, and a
        croak must never happen while holding the lock. */
     kp = SvPVbyte(key, klen);
     REEXTRACT(self);
     rdx_rwlock_wrlock(h);
+    if (h->hdr->sealed) { rdx_rwlock_wrunlock(h); croak("Data::RadixTree::Shared->insert: tree is frozen (read-only)"); }
     if (!rdx_insert_has_room(h, (uint32_t)klen)) {
         rdx_rwlock_wrunlock(h);   /* release BEFORE croak */
         croak("Data::RadixTree::Shared->insert: capacity exhausted "
@@ -134,9 +160,13 @@ lookup(self, key)
   CODE:
     kp = SvPVbyte(key, klen);   /* before the lock */
     REEXTRACT(self);
-    rdx_rwlock_rdlock(h);
-    found = rdx_lookup_locked(h, (const uint8_t *)kp, (uint32_t)klen, &val);
-    rdx_rwlock_rdunlock(h);
+    if (h->readonly) {          /* frozen: immutable tree, no lock needed */
+        found = rdx_lookup_locked(h, (const uint8_t *)kp, (uint32_t)klen, &val);
+    } else {
+        rdx_rwlock_rdlock(h);
+        found = rdx_lookup_locked(h, (const uint8_t *)kp, (uint32_t)klen, &val);
+        rdx_rwlock_rdunlock(h);
+    }
     RETVAL = found ? newSVuv((UV)val) : &PL_sv_undef;
   OUTPUT:
     RETVAL
@@ -152,9 +182,13 @@ exists(self, key)
   CODE:
     kp = SvPVbyte(key, klen);   /* before the lock */
     REEXTRACT(self);
-    rdx_rwlock_rdlock(h);
-    RETVAL = rdx_lookup_locked(h, (const uint8_t *)kp, (uint32_t)klen, NULL) ? 1 : 0;
-    rdx_rwlock_rdunlock(h);
+    if (h->readonly) {          /* frozen: immutable tree, no lock needed */
+        RETVAL = rdx_lookup_locked(h, (const uint8_t *)kp, (uint32_t)klen, NULL) ? 1 : 0;
+    } else {
+        rdx_rwlock_rdlock(h);
+        RETVAL = rdx_lookup_locked(h, (const uint8_t *)kp, (uint32_t)klen, NULL) ? 1 : 0;
+        rdx_rwlock_rdunlock(h);
+    }
   OUTPUT:
     RETVAL
 
@@ -171,9 +205,13 @@ longest_prefix(self, key)
   CODE:
     kp = SvPVbyte(key, klen);   /* before the lock */
     REEXTRACT(self);
-    rdx_rwlock_rdlock(h);
-    found = rdx_longest_prefix_locked(h, (const uint8_t *)kp, (uint32_t)klen, &val);
-    rdx_rwlock_rdunlock(h);
+    if (h->readonly) {          /* frozen: immutable tree, no lock needed */
+        found = rdx_longest_prefix_locked(h, (const uint8_t *)kp, (uint32_t)klen, &val);
+    } else {
+        rdx_rwlock_rdlock(h);
+        found = rdx_longest_prefix_locked(h, (const uint8_t *)kp, (uint32_t)klen, &val);
+        rdx_rwlock_rdunlock(h);
+    }
     RETVAL = found ? newSVuv((UV)val) : &PL_sv_undef;
   OUTPUT:
     RETVAL
@@ -188,9 +226,11 @@ delete(self, key)
     const char *kp;
     int removed;
   CODE:
+    if (h->readonly) croak("Data::RadixTree::Shared->delete: tree is frozen (read-only)");
     kp = SvPVbyte(key, klen);   /* before the lock */
     REEXTRACT(self);
     rdx_rwlock_wrlock(h);
+    if (h->hdr->sealed) { rdx_rwlock_wrunlock(h); croak("Data::RadixTree::Shared->delete: tree is frozen (read-only)"); }
     removed = rdx_delete_locked(h, (const uint8_t *)kp, (uint32_t)klen);
     __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
     rdx_rwlock_wrunlock(h);
@@ -204,10 +244,42 @@ clear(self)
   PREINIT:
     EXTRACT(self);
   CODE:
+    if (h->readonly) croak("Data::RadixTree::Shared->clear: tree is frozen (read-only)");
     rdx_rwlock_wrlock(h);
+    if (h->hdr->sealed) { rdx_rwlock_wrunlock(h); croak("Data::RadixTree::Shared->clear: tree is frozen (read-only)"); }
     rdx_clear_locked(h);
     __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
     rdx_rwlock_wrunlock(h);
+
+void
+freeze(self)
+    SV *self
+  PREINIT:
+    EXTRACT(self);
+  CODE:
+    if (h->readonly) croak("Data::RadixTree::Shared->freeze: cannot freeze a read-only handle");
+    if (rdx_freeze(h) != 0) croak("Data::RadixTree::Shared->freeze: msync: %s", strerror(errno));
+    h->readonly = 1;   /* this handle now rejects mutation too (the file is sealed) */
+
+UV
+frozen(self)
+    SV *self
+  PREINIT:
+    EXTRACT(self);
+  CODE:
+    RETVAL = h->hdr->sealed ? 1 : 0;
+  OUTPUT:
+    RETVAL
+
+UV
+readonly(self)
+    SV *self
+  PREINIT:
+    EXTRACT(self);
+  CODE:
+    RETVAL = h->readonly ? 1 : 0;
+  OUTPUT:
+    RETVAL
 
 UV
 count(self)
@@ -215,9 +287,13 @@ count(self)
   PREINIT:
     EXTRACT(self);
   CODE:
-    rdx_rwlock_rdlock(h);
-    RETVAL = (UV)h->hdr->keys;
-    rdx_rwlock_rdunlock(h);
+    if (h->readonly) {          /* frozen: immutable tree, no lock needed */
+        RETVAL = (UV)h->hdr->keys;
+    } else {
+        rdx_rwlock_rdlock(h);
+        RETVAL = (UV)h->hdr->keys;
+        rdx_rwlock_rdunlock(h);
+    }
   OUTPUT:
     RETVAL
 
@@ -232,14 +308,14 @@ stats(self)
         uint32_t node_used, node_cap, arena_used, arena_cap;
         /* Snapshot under the lock; do all (croak-capable) Perl allocation after
            releasing it -- so an OOM in newHV/newSV* can never strand the lock. */
-        rdx_rwlock_rdlock(h);
+        if (!h->readonly) rdx_rwlock_rdlock(h);   /* frozen: immutable, no lock */
         keys       = h->hdr->keys;
         node_used  = h->hdr->node_used;
         node_cap   = h->hdr->node_cap;
         arena_used = h->hdr->arena_used;
         arena_cap  = h->hdr->arena_cap;
         ops        = h->hdr->stat_ops;
-        rdx_rwlock_rdunlock(h);
+        if (!h->readonly) rdx_rwlock_rdunlock(h);
 
         HV *hv = newHV();
         hv_stores(hv, "keys",           newSVuv((UV)keys));
@@ -249,6 +325,8 @@ stats(self)
         hv_stores(hv, "arena_capacity", newSVuv((UV)arena_cap));
         hv_stores(hv, "ops",            newSVuv((UV)ops));
         hv_stores(hv, "mmap_size",      newSVuv((UV)h->mmap_size));
+        hv_stores(hv, "frozen",         newSVuv(h->hdr->sealed ? 1 : 0));
+        hv_stores(hv, "readonly",       newSVuv(h->readonly ? 1 : 0));
         RETVAL = newRV_noinc((SV *)hv);
     }
   OUTPUT:
@@ -280,7 +358,7 @@ sync(self)
   PREINIT:
     EXTRACT(self);
   CODE:
-    if (rdx_msync(h) != 0) croak("sync: %s", strerror(errno));
+    if (!h->readonly && rdx_msync(h) != 0) croak("sync: %s", strerror(errno));
 
 void
 unlink(self, ...)
@@ -288,7 +366,12 @@ unlink(self, ...)
   CODE:
     if (sv_isobject(self) && sv_derived_from(self, "Data::RadixTree::Shared")) {
         RdxHandle *h = INT2PTR(RdxHandle*, SvIV(SvRV(self)));
-        if (h && h->path) unlink(h->path);
+        if (h && h->path && unlink(h->path) != 0 && errno != ENOENT)
+            croak("Data::RadixTree::Shared->unlink(%s): %s", h->path, strerror(errno));
     } else if (items >= 2 && (SvGETMAGIC(ST(1)), SvOK(ST(1)))) {
-        unlink(SvPV_nolen(ST(1)));
+        {
+            const char *up = SvPV_nolen(ST(1));
+            if (unlink(up) != 0 && errno != ENOENT)
+                croak("Data::RadixTree::Shared->unlink(%s): %s", up, strerror(errno));
+        }
     }

vividsnow/perl5-data-roaringbitmap-shared

Shared.xs
@@ -10,6 +10,7 @@
         croak("Expected a Data::RoaringBitmap::Shared object"); \
     RbHandle *h = INT2PTR(RbHandle*, SvIV(SvRV(sv))); \
     if (!h) croak("Attempted to use a destroyed Data::RoaringBitmap::Shared object"); \
+    RbHandle *h0 = h; PERL_UNUSED_VAR(h0); \
     sv_2mortal(SvREFCNT_inc(SvRV(sv)))
 
 /* Re-read the handle after a call that can run Perl code (tied/overloaded
@@ -22,7 +23,7 @@
     if (!SvROK(sv)) \
         croak("Data::RoaringBitmap::Shared object was replaced during the call"); \
     h = INT2PTR(RbHandle*, SvIV(SvRV(sv))); \
-    if (!h) croak("Data::RoaringBitmap::Shared object destroyed during the call")
+    if (h != h0) croak("Data::RoaringBitmap::Shared object replaced or destroyed during the call")
 
 #define MAKE_OBJ(class, handle) \
     SV *obj = newSViv(PTR2IV(handle)); \
@@ -74,7 +75,7 @@ new(class, path = &PL_sv_undef, container_capacity = 256, file_mode = 0600)
   CODE:
     const char *p = (SvGETMAGIC(path), SvOK(path)) ? SvPV_nolen(path) : NULL;
     RbHandle *h = rb_create(p, (uint64_t)container_capacity, (mode_t)file_mode, errbuf);   /* validates args into errbuf */
-    if (!h) croak("Data::RoaringBitmap::Shared->new: %s", errbuf);
+    if (!h) croak("Data::RoaringBitmap::Shared->new: %s", errbuf[0] ? errbuf : "out of memory");
     MAKE_OBJ(class, h);
   OUTPUT:
     RETVAL
@@ -89,7 +90,7 @@ new_memfd(class, name = &PL_sv_undef, container_capacity = 256)
   CODE:
     const char *nm = (SvGETMAGIC(name), SvOK(name)) ? SvPV_nolen(name) : NULL;   /* undef -> default label */
     RbHandle *h = rb_create_memfd(nm, (uint64_t)container_capacity, errbuf);   /* validates args into errbuf */
-    if (!h) croak("Data::RoaringBitmap::Shared->new_memfd: %s", errbuf);
+    if (!h) croak("Data::RoaringBitmap::Shared->new_memfd: %s", errbuf[0] ? errbuf : "out of memory");
     MAKE_OBJ(class, h);
   OUTPUT:
     RETVAL
@@ -102,7 +103,28 @@ new_from_fd(class, fd)
     char errbuf[RB_ERR_BUFLEN];
   CODE:
     RbHandle *h = rb_open_fd(fd, errbuf);
-    if (!h) croak("Data::RoaringBitmap::Shared->new_from_fd: %s", errbuf);
+    if (!h) croak("Data::RoaringBitmap::Shared->new_from_fd: %s", errbuf[0] ? errbuf : "out of memory");
+    MAKE_OBJ(class, h);
+  OUTPUT:
+    RETVAL
+
+SV *
+new_readonly(class, path)
+    const char *class
+    SV *path
+  PREINIT:
+    char errbuf[RB_ERR_BUFLEN];
+  CODE:
+    /* Open a FROZEN (sealed) file read-only: O_RDONLY + PROT_READ, no lock
+       ever.  A sealed file is immutable and no read path writes the mapping,
+       so queries take no reader-slot / rwlock traffic and any number of
+       processes can share one PROT_READ mapping (same architecture; the
+       magic rejects a wrong-endian file). */
+    const char *p = (SvGETMAGIC(path), SvOK(path)) ? SvPV_nolen(path) : NULL;
+    if (!p) croak("Data::RoaringBitmap::Shared->new_readonly: path is required");
+    RbHandle *h = rb_open_readonly(p, errbuf);
+    if (!h) croak("Data::RoaringBitmap::Shared->new_readonly: %s", errbuf);
+    class = SvPV_nolen(ST(0));   /* re-read: path get-magic may have realloced ST(0)'s PV before bless */
     MAKE_OBJ(class, h);
   OUTPUT:
     RETVAL
@@ -125,11 +147,13 @@ add(self, x)
     int added;
     uint32_t hi;
   CODE:
+    if (h->readonly) croak("Data::RoaringBitmap::Shared->add: bitmap is frozen (read-only)");
     /* Range-check BEFORE locking: a croak must never happen under the lock. */
     if (x > RB_UINT32_MAX_UV)
         croak("Data::RoaringBitmap::Shared->add: value %" UVuf " exceeds uint32 (max 4294967295)", x);
     hi = (uint32_t)(x >> 16);
     rb_rwlock_wrlock(h);
+    if (h->hdr->sealed) { rb_rwlock_wrunlock(h); croak("Data::RoaringBitmap::Shared->add: bitmap is frozen (read-only)"); }
     /* Need a free container slot only if this value touches an empty bucket. */
     if (rb_buckets(h)[hi].type == RB_TYPE_NONE && rb_avail_slots(h) == 0) {
         __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);   /* a call that took the write lock counts (matches add_many) */
@@ -155,10 +179,12 @@ add_many(self, ints)
     UV *vals;
     IV total_added;
   CODE:
+    if (h->readonly) croak("Data::RoaringBitmap::Shared->add_many: bitmap is frozen (read-only)");
     SvGETMAGIC(ints);
     if (!SvROK(ints) || SvTYPE(SvRV(ints)) != SVt_PVAV)
         croak("Data::RoaringBitmap::Shared->add_many: expected an array reference");
     av = (AV *)SvRV(ints);
+    sv_2mortal(SvREFCNT_inc((SV *)av));   /* pin the arrayref: element magic below cannot free it mid-loop */
     n = av_len(av) + 1;
     /* Resolve + range-check every value BEFORE the lock (croak-free section). */
     Newx(vals, n > 0 ? n : 1, UV);
@@ -176,6 +202,7 @@ add_many(self, ints)
     total_added = 0;
     REEXTRACT(self);
     rb_rwlock_wrlock(h);
+    if (h->hdr->sealed) { rb_rwlock_wrunlock(h); croak("Data::RoaringBitmap::Shared->add_many: bitmap is frozen (read-only)"); }
     for (i = 0; i < n; i++) {
         uint32_t hi = (uint32_t)(vals[i] >> 16);
         if (rb_buckets(h)[hi].type == RB_TYPE_NONE && rb_avail_slots(h) == 0) {
@@ -200,6 +227,9 @@ contains(self, x)
     EXTRACT(self);
   CODE:
     if (x > RB_UINT32_MAX_UV) { RETVAL = 0; }   /* a value out of range is simply absent */
+    else if (h->readonly) {                     /* frozen: immutable containers, no lock */
+        RETVAL = rb_contains_locked(h, (uint32_t)x) ? 1 : 0;
+    }
     else {
         rb_rwlock_rdlock(h);
         RETVAL = rb_contains_locked(h, (uint32_t)x) ? 1 : 0;
@@ -216,9 +246,11 @@ remove(self, x)
     EXTRACT(self);
     int removed;
   CODE:
+    if (h->readonly) croak("Data::RoaringBitmap::Shared->remove: bitmap is frozen (read-only)");
     if (x > RB_UINT32_MAX_UV) { RETVAL = 0; }   /* out of range -> nothing to remove */
     else {
         rb_rwlock_wrlock(h);
+        if (h->hdr->sealed) { rb_rwlock_wrunlock(h); croak("Data::RoaringBitmap::Shared->remove: bitmap is frozen (read-only)"); }
         removed = rb_remove_locked(h, (uint32_t)x);
         __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
         rb_rwlock_wrunlock(h);
@@ -233,9 +265,13 @@ cardinality(self)
   PREINIT:
     EXTRACT(self);
   CODE:
-    rb_rwlock_rdlock(h);
-    RETVAL = (UV)h->hdr->cardinality;
-    rb_rwlock_rdunlock(h);
+    if (h->readonly) {                          /* frozen: immutable, no lock */
+        RETVAL = (UV)h->hdr->cardinality;
+    } else {
+        rb_rwlock_rdlock(h);
+        RETVAL = (UV)h->hdr->cardinality;
+        rb_rwlock_rdunlock(h);
+    }
   OUTPUT:
     RETVAL
 
@@ -245,9 +281,13 @@ is_empty(self)
   PREINIT:
     EXTRACT(self);
   CODE:
-    rb_rwlock_rdlock(h);
-    RETVAL = (h->hdr->cardinality == 0) ? 1 : 0;
-    rb_rwlock_rdunlock(h);
+    if (h->readonly) {                          /* frozen: immutable, no lock */
+        RETVAL = (h->hdr->cardinality == 0) ? 1 : 0;
+    } else {
+        rb_rwlock_rdlock(h);
+        RETVAL = (h->hdr->cardinality == 0) ? 1 : 0;
+        rb_rwlock_rdunlock(h);
+    }
   OUTPUT:
     RETVAL
 
@@ -259,9 +299,13 @@ min(self)
     uint32_t v;
     int found;
   CODE:
-    rb_rwlock_rdlock(h);
-    found = rb_min_locked(h, &v);
-    rb_rwlock_rdunlock(h);
+    if (h->readonly) {                          /* frozen: immutable, no lock */
+        found = rb_min_locked(h, &v);
+    } else {
+        rb_rwlock_rdlock(h);
+        found = rb_min_locked(h, &v);
+        rb_rwlock_rdunlock(h);
+    }
     RETVAL = found ? newSVuv((UV)v) : &PL_sv_undef;
   OUTPUT:
     RETVAL
@@ -274,9 +318,13 @@ max(self)
     uint32_t v;
     int found;
   CODE:
-    rb_rwlock_rdlock(h);
-    found = rb_max_locked(h, &v);
-    rb_rwlock_rdunlock(h);
+    if (h->readonly) {                          /* frozen: immutable, no lock */
+        found = rb_max_locked(h, &v);
+    } else {
+        rb_rwlock_rdlock(h);
+        found = rb_max_locked(h, &v);
+        rb_rwlock_rdunlock(h);
+    }
     RETVAL = found ? newSVuv((UV)v) : &PL_sv_undef;
   OUTPUT:
     RETVAL
@@ -296,16 +344,20 @@ to_array(self)
        while the lock is held.  The cardinality may grow between the read and the
        fill, but to_array is a best-effort snapshot; we cap the fill at the buffer
        length so we never write past it. */
-    rb_rwlock_rdlock(h);
-    card = (UV)h->hdr->cardinality;
-    rb_rwlock_rdunlock(h);
+    if (h->readonly) {                          /* frozen: immutable, no lock */
+        card = (UV)h->hdr->cardinality;
+    } else {
+        rb_rwlock_rdlock(h);
+        card = (UV)h->hdr->cardinality;
+        rb_rwlock_rdunlock(h);
+    }
 
     {
         uint32_t *buf = NULL;
         RbBucket *bt;
         UV idx = 0;
         if (card) { Newx(buf, card, uint32_t); SAVEFREEPV(buf); }
-        rb_rwlock_rdlock(h);
+        if (!h->readonly) rb_rwlock_rdlock(h);
         bt = rb_buckets(h);
         for (uint32_t hi = 0; hi < RB_NUM_BUCKETS && idx < card; hi++) {
             if (bt[hi].type == RB_TYPE_NONE) continue;
@@ -326,7 +378,7 @@ to_array(self)
                 }
             }
         }
-        rb_rwlock_rdunlock(h);
+        if (!h->readonly) rb_rwlock_rdunlock(h);
 
         av = newAV();
         if (idx) {
@@ -346,6 +398,7 @@ union(self, other)
   PREINIT:
     EXTRACT(self);
   CODE:
+    if (h->readonly) croak("Data::RoaringBitmap::Shared->union: bitmap is frozen (read-only)");
     if (!sv_isobject(other) || !sv_derived_from(other, "Data::RoaringBitmap::Shared"))
         croak("Data::RoaringBitmap::Shared->union: expected a Data::RoaringBitmap::Shared object");
     {
@@ -363,9 +416,30 @@ union(self, other)
             __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
             SvREFCNT_inc(self);
             RETVAL = self;
+        } else if (o->readonly) {
+            /* Frozen other: its containers are immutable (PROT_READ), so read them
+             * lock-free -- take ONLY the receiver's write lock (a read lock on o's
+             * PROT_READ mapping would SIGSEGV).  No lock ordering is needed with
+             * one lock, so no rb_lock_pair. */
+            uint32_t need;
+            rb_rwlock_wrlock(h);
+            if (h->hdr->sealed) { rb_rwlock_wrunlock(h); croak("Data::RoaringBitmap::Shared->union: bitmap is frozen (read-only)"); }
+            need = rb_union_new_slots_needed(h, o);
+            if (rb_avail_slots(h) < need) {
+                __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
+                rb_rwlock_wrunlock(h);      /* release BEFORE croak */
+                croak("Data::RoaringBitmap::Shared->union: container pool exhausted "
+                      "(needs %u more container(s); grow container_capacity)", need);
+            }
+            rb_union_locked(h, o);
+            __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
+            rb_rwlock_wrunlock(h);
+            SvREFCNT_inc(self);
+            RETVAL = self;
         } else {
             uint32_t need;
             rb_lock_pair(h, o);
+            if (h->hdr->sealed) { rb_unlock_pair(h, o); croak("Data::RoaringBitmap::Shared->union: bitmap is frozen (read-only)"); }
             need = rb_union_new_slots_needed(h, o);
             if (rb_avail_slots(h) < need) {
                 __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
@@ -390,6 +464,7 @@ intersect(self, other)
   PREINIT:
     EXTRACT(self);
   CODE:
+    if (h->readonly) croak("Data::RoaringBitmap::Shared->intersect: bitmap is frozen (read-only)");
     if (!sv_isobject(other) || !sv_derived_from(other, "Data::RoaringBitmap::Shared"))
         croak("Data::RoaringBitmap::Shared->intersect: expected a Data::RoaringBitmap::Shared object");
     {
@@ -406,8 +481,20 @@ intersect(self, other)
             __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
             SvREFCNT_inc(self);
             RETVAL = self;
+        } else if (o->readonly) {
+            /* Frozen other: immutable containers, read lock-free -- take ONLY the
+             * receiver's write lock (o is PROT_READ; an rdlock on it would
+             * SIGSEGV).  intersect never needs new slots. */
+            rb_rwlock_wrlock(h);
+            if (h->hdr->sealed) { rb_rwlock_wrunlock(h); croak("Data::RoaringBitmap::Shared->intersect: bitmap is frozen (read-only)"); }
+            rb_intersect_locked(h, o);
+            __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
+            rb_rwlock_wrunlock(h);
+            SvREFCNT_inc(self);
+            RETVAL = self;
         } else {
             rb_lock_pair(h, o);             /* intersect never needs new slots */
+            if (h->hdr->sealed) { rb_unlock_pair(h, o); croak("Data::RoaringBitmap::Shared->intersect: bitmap is frozen (read-only)"); }
             rb_intersect_locked(h, o);
             __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
             rb_unlock_pair(h, o);
@@ -424,10 +511,42 @@ clear(self)
   PREINIT:
     EXTRACT(self);
   CODE:
+    if (h->readonly) croak("Data::RoaringBitmap::Shared->clear: bitmap is frozen (read-only)");
     rb_rwlock_wrlock(h);
+    if (h->hdr->sealed) { rb_rwlock_wrunlock(h); croak("Data::RoaringBitmap::Shared->clear: bitmap is frozen (read-only)"); }
     rb_clear_locked(h);
     __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
     rb_rwlock_wrunlock(h);
+
+void
+freeze(self)
+    SV *self
+  PREINIT:
+    EXTRACT(self);
+  CODE:
+    if (h->readonly) croak("Data::RoaringBitmap::Shared->freeze: cannot freeze a read-only handle");
+    if (rb_freeze(h) != 0) croak("Data::RoaringBitmap::Shared->freeze: msync: %s", strerror(errno));
+    h->readonly = 1;   /* this handle now rejects mutation too (the file is sealed) */
+
+UV
+frozen(self)
+    SV *self
+  PREINIT:
+    EXTRACT(self);
+  CODE:
+    RETVAL = h->hdr->sealed ? 1 : 0;
+  OUTPUT:
+    RETVAL
+
+UV
+readonly(self)
+    SV *self
+  PREINIT:
+    EXTRACT(self);
+  CODE:
+    RETVAL = h->readonly ? 1 : 0;
+  OUTPUT:
+    RETVAL
 
 SV *
 stats(self)
@@ -440,13 +559,13 @@ stats(self)
         uint32_t cont_used, cont_cap, bkts_used;
         /* Snapshot under the lock; do all (croak-capable) Perl allocation after
            releasing it -- so an OOM in newHV/newSV* can never strand the lock. */
-        rb_rwlock_rdlock(h);
+        if (!h->readonly) rb_rwlock_rdlock(h);   /* frozen: immutable, no lock */
         card      = h->hdr->cardinality;
         cont_used = h->hdr->container_used;
         cont_cap  = h->hdr->container_cap;
         ops       = h->hdr->stat_ops;
         bkts_used = rb_buckets_used(h);
-        rb_rwlock_rdunlock(h);
+        if (!h->readonly) rb_rwlock_rdunlock(h);
 
         HV *hv = newHV();
         hv_stores(hv, "cardinality",         newSVuv((UV)card));
@@ -455,6 +574,8 @@ stats(self)
         hv_stores(hv, "buckets_used",        newSVuv((UV)bkts_used));
         hv_stores(hv, "ops",                 newSVuv((UV)ops));
         hv_stores(hv, "mmap_size",           newSVuv((UV)h->mmap_size));
+        hv_stores(hv, "frozen",              newSVuv(h->hdr->sealed ? 1 : 0));
+        hv_stores(hv, "readonly",            newSVuv(h->readonly ? 1 : 0));
         RETVAL = newRV_noinc((SV *)hv);
     }
   OUTPUT:
@@ -486,7 +607,7 @@ sync(self)
   PREINIT:
     EXTRACT(self);
   CODE:
-    if (rb_msync(h) != 0) croak("sync: %s", strerror(errno));
+    if (!h->readonly && rb_msync(h) != 0) croak("sync: %s", strerror(errno));
 
 void
 unlink(self, ...)
@@ -494,7 +615,12 @@ unlink(self, ...)
   CODE:
     if (sv_isobject(self) && sv_derived_from(self, "Data::RoaringBitmap::Shared")) {
         RbHandle *h = INT2PTR(RbHandle*, SvIV(SvRV(self)));
-        if (h && h->path) unlink(h->path);
+        if (h && h->path && unlink(h->path) != 0 && errno != ENOENT)
+            croak("Data::RoaringBitmap::Shared->unlink(%s): %s", h->path, strerror(errno));
     } else if (items >= 2 && (SvGETMAGIC(ST(1)), SvOK(ST(1)))) {
-        unlink(SvPV_nolen(ST(1)));
+        {
+            const char *up = SvPV_nolen(ST(1));
+            if (unlink(up) != 0 && errno != ENOENT)
+                croak("Data::RoaringBitmap::Shared->unlink(%s): %s", up, strerror(errno));
+        }
     }